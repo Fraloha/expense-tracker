8d793e295a53af7513643d8b3ea282cc49e57e3f
Fraloha <infofrancesco20@gmail.com>
2020-06-15 15:00:00 +0200

    Defined ExpenseDTOs class for expenses data manipulation

---
+++ expense-tracker/src/main/java/com/expensetracker/dto/ExpenseDTOs.java
@@ -0,0 +1,14 @@
+package com.expensetracker.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ExpenseDTOs {
+
+    
+}
