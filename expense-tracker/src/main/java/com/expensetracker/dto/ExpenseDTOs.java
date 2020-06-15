3fd1a8ed62783f0442960ca0970faca578a9059a
Fraloha <infofrancesco20@gmail.com>
2020-06-15 15:45:12 +0200

    Implemented Request and Response static classes

--- expense-tracker/src/main/java/com/expensetracker/dto/ExpenseDTOs.java
+++ expense-tracker/src/main/java/com/expensetracker/dto/ExpenseDTOs.java
@@ -10,5 +10,37 @@ import java.time.LocalDate;
 
 public class ExpenseDTOs {
 
+    @Data
+    public static class ExpenseRequest {
+        @NotBlank
+        private String description;
+
+        @NotNull
+        @Positive
+        private BigDecimal amount;
+
+        @NotNull
+        private LocalDate date;
+
+        private Long categoryId;
+    }
+
+    @Data
+    public static class ExpenseResponse {
+        private Long id;
+        private String description;
+        private BigDecimal amount;
+        private LocalDate date;
+        private String categoryName;
+
+        public ExpenseResponse(Long id, String description, BigDecimal amount,
+                               LocalDate date, String categoryName) {
+            this.id = id;
+            this.description = description;
+            this.amount = amount;
+            this.date = date;
+            this.categoryName = categoryName;
+        }
+    }
     
 }
