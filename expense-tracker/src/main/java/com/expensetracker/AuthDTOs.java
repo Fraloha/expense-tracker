d3bfa3d64b4c392a83ac469ef39df9788b8d09b0
Fraloha <infofrancesco20@gmail.com>
2020-06-15 12:10:16 +0200

    Define AuthDTOs (utility DTO class)

---
+++ expense-tracker/src/main/java/com/expensetracker/AuthDTOs.java
@@ -0,0 +1,10 @@
+package com.expensetracker.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+public class AuthDTOs {
+
+    
+}
