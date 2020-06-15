ca71d71bbe8b52593ec83ed109e4b20e0e06f900
Fraloha <infofrancesco20@gmail.com>
2020-06-15 12:10:16 +0200

    Defining the expense object (very simple and straightforward).

---
+++ expense-tracker/src/main/java/com/expensetracker/model/Expense.java
@@ -0,0 +1,44 @@
+package com.expensetracker.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "expenses")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    @Positive
+    private BigDecimal amount;
+
+    @NotNull
+    private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
