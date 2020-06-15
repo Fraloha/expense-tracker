26d1a2108bd3f7551d772bfb3749e3547eb092ae
Fraloha <infofrancesco20@gmail.com>
2020-06-15 12:10:16 +0200

    Defined static classes for JSON data manipulation (Login, Register, Authentication)

--- expense-tracker/src/main/java/com/expensetracker/AuthDTOs.java
+++ expense-tracker/src/main/java/com/expensetracker/AuthDTOs.java
@@ -18,4 +18,26 @@ public class AuthDTOs {
         @NotBlank
         private String password;
     }
+
+     @Data
+    public static class LoginRequest {
+        @NotBlank
+        private String username;
+
+        @NotBlank
+        private String password;
+    }
+
+    @Data
+    public static class AuthResponse {
+        private String token;
+        private String username;
+        private String email;
+
+        public AuthResponse(String token, String username, String email) {
+            this.token = token;
+            this.username = username;
+            this.email = email;
+        }
+    }
 }
