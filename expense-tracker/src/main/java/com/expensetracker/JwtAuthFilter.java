ae28760de48626d201599e7192e8712a4174ffde
Fraloha <infofrancesco20@gmail.com>
2020-06-17 11:31:00 +0200

    Defined Authentication class and request parsing method

---
+++ expense-tracker/src/main/java/com/expensetracker/JwtAuthFilter.java
@@ -0,0 +1,24 @@
+package com.expensetracker.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final UserDetailsService userDetailsService;
+
+    private String parseJwt(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (StringUtils.hasText(header) && header.startsWith("Bearer ")) {
+            return header.substring(7);
+        }
+        return null;
+    }
+}
